bd3cb28576aa787c813a16c4003fe8a15c020581
Ngayka <ngayka@gmail.com>
2024-12-01 20:13:11 +0200

    Can`t find solution because of wifes and husbands

--- app/main.py
+++ app/main.py
@@ -4,15 +4,13 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
-        self.husband = None
-        self.wife = None
 
         Person.people[self.name] = self
 
-    def add_husband(self, husband: str) -> None:
+    def add_husband(self, husband: "Person") -> None:
         self.husband = husband
 
-    def add_wife(self, wife: str) -> None:
+    def add_wife(self, wife: "Person") -> None:
         self.wife = wife
 
 
@@ -22,18 +20,30 @@ def create_person_list(people: list) -> list:
     for one in people:
         name = one.get("name")
         age = one.get("age")
-        husband = one.get("husband")
-        wife = one.get("wife")
+        husband_name = one.get("husband")
+        wife_name = one.get("wife")
 
         if name in Person.people:
             person = Person.people[name]
         else:
             person = Person(name=name, age=age)
-        person_list.append(person)
-        if husband and husband != person.name:
+            person_list.append(person)
+
+        if husband_name:
+            if husband_name in Person.people:
+                husband = Person.people[husband_name]
+            else:
+                husband = Person(name=husband_name, age=0)
+                person_list.append(husband)
             person.add_husband(husband)
 
-        if wife and wife != person.name:
+        if wife_name:
+            if wife_name in Person.people:
+                wife = Person.people[wife_name]
+            else:
+                wife = Person(name=wife_name, age=0)
+                person_list.append(wife)
             person.add_wife(wife)
-    return person_list
+
+    return list(Person.people.values())
 
