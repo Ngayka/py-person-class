b47e5a73a299ad876e38b254dc2058f07b5c67dc
Ngayka <ngayka@gmail.com>
2024-12-11 19:07:55 +0200

    find solution, all tests are rigth, but 1 error is

--- app/main.py
+++ app/main.py
@@ -4,46 +4,28 @@ class Person:
     def __init__(self, name: str, age: int) -> None:
         self.name = name
         self.age = age
+        self.wife = None
+        self.husband = None
 
-        Person.people[self.name] = self
-
-    def add_husband(self, husband: "Person") -> None:
-        self.husband = husband
-
-    def add_wife(self, wife: "Person") -> None:
-        self.wife = wife
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    person_list = []
+def create_person_list(people_list: list) -> list:
+    persons = []
 
-    for one in people:
-        name = one.get("name")
-        age = one.get("age")
-        husband_name = one.get("husband")
-        wife_name = one.get("wife")
+    for one in people_list:
+        name = one["name"]
+        age = one["age"]
+        person = Person(name, age)
+        persons.append(person)
 
-        if name in Person.people:
-            person = Person.people[name]
-        else:
-            person = Person(name=name, age=age)
-            person_list.append(person)
+    for one in people_list:
+        name = one["name"]
+        person = Person.people[name]
 
-        if husband_name:
-            if husband_name in Person.people:
-                husband = Person.people[husband_name]
-            else:
-                husband = Person(name=husband_name, age=0)
-                person_list.append(husband)
-            person.add_husband(husband)
+        if "wife" in one and one["wife"]:
+            person.wife = Person.people.get(one["wife"])
+        elif "husband" in one and one["husband"]:
+            person.husband = Person.people.get(one["husband"])
 
-        if wife_name:
-            if wife_name in Person.people:
-                wife = Person.people[wife_name]
-            else:
-                wife = Person(name=wife_name, age=0)
-                person_list.append(wife)
-            person.add_wife(wife)
-
-    return list(Person.people.values())
-
+    return persons
